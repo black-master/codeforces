021b105faa3480905d825c7cc9902f3f3d6858f5
Zubayer Rahman <zubayer.csesust@gmail.com>
2016-09-08 22:30:37 +0600

    Create 630J.cpp

---
+++ 630J.cpp
@@ -0,0 +1,23 @@
+/**
+Author : Zubayer Rahman
+Email  : zubayer.csesust@gmail.com
+CPU    : 0.015s
+Memory : 0KB
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    long long n,a;
+
+    while(cin>>n)
+    {
+        a=n/2520;
+
+        cout<<a<<endl;
+    }
+    return 0;
+}
