a14027960a6a398cc52db2a2cc516012551bc629
Zubayer Rahman <zubayer.csesust@gmail.com>
2017-12-26 17:53:30 +0600

    Update 1A.cpp

--- 1A.cpp
+++ 1A.cpp
@@ -1,43 +1,39 @@
 /**
-Author : Zubayer Rahman
-Email  : zubayer.csesust@gmail.com
-CPU    : 0.031s
-Memory : 0KB
+Online Judge : CodeForces
+Problem No   : 1A
+Problem Name : Theatre Square
+Author       : Zubayer Rahman
+Email        : zubayer.csesust@gmail.com
+CPU          : 0.031s
+Memory       : 12KB
+Algorithm    : Implementation
 */
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<iostream>
-#include<algorithm>
+#include<bits/stdc++.h>
 
 using namespace std;
 
+long long n,m,a;
+
 int main()
 {
-    long long n,m,a;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif // ONLINE_JUDGE
 
     while(cin>>n>>m>>a)
     {
         long long _n,_m,_a;
 
         if(n%a==0)
-        {
             _n=n/a;
-        }
         else
-        {
             _n=(n/a)+1;
-        }
 
         if(m%a==0)
-        {
             _m=m/a;
-        }
         else
-        {
             _m=(m/a)+1;
-        }
 
         _a=_n*_m;
 
